893c2f59d861f82934e77072b995c40645fa245c
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-23 01:59:08 -0700

    basic 3d model

--- components/EZCheck.tsx
+++ components/EZCheck.tsx
@@ -1,27 +1,59 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, extend } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Flex } from "@chakra-ui/react";
+import { TextGeometry } from "three/examples/jsm/Addons.js";
 
-function LED() {}
+const boxWidth = 1.13;
+const boxHeight = 1.35;
+const boxDepth = 0.5;
+
+const ledRadius = boxDepth / 2 - 0.05;
+const ledHeight = 0.2;
+
+const lcdPad = 0.1;
+const lcdWidth = boxWidth - 2 * lcdPad;
+const lcdHeight = lcdWidth / 3;
+const lcdDepth = 0.05;
+
+// const gridLen = 0.75;
+const gridLen = boxHeight - 3 * lcdPad - lcdHeight;
+
+type LEDProps = {
+  left: boolean;
+};
+function LED(props: LEDProps) {
+  return (
+    <mesh
+      position={[
+        (props.left ? -1 : 1) * (boxWidth / 2) +
+          (props.left ? 1 : -1) * ledRadius,
+        boxHeight / 2 + ledHeight / 2,
+        0,
+      ]}
+    >
+      <cylinderGeometry args={[ledRadius, ledRadius, ledHeight]} />
+      <meshStandardMaterial color={props.left ? "#FF0000" : "#00FF00"} />
+    </mesh>
+  );
+}
 function Frame() {
-  const boxWidth = 1.13;
-  const boxHeight = 1.35;
-  const boxDepth = 0.5;
-
-  const ledRadius = boxDepth / 2;
-  const ledHeight = 0.3;
-
   return (
     <mesh position={[0, 0, 0]}>
       <boxGeometry args={[boxWidth, boxHeight, boxDepth]} />
-      <meshStandardMaterial />
+      <meshStandardMaterial color="#CBD5E0" />
+      <LED left={true} />
+      <LED left={false} />
       <mesh
-        position={[-boxWidth / 2 + ledRadius, boxHeight / 2 + ledHeight / 2, 0]}
+        position={[0, boxHeight / 2 - lcdHeight / 2 - lcdPad, boxDepth / 2]}
       >
-        <cylinderGeometry args={[ledRadius, ledRadius, ledHeight]} />
-        <meshStandardMaterial />
+        <boxGeometry args={[lcdWidth, lcdHeight, lcdDepth]} />
+        <meshStandardMaterial color={"#0000FF"} />
+      </mesh>
+      <mesh position={[0, -boxHeight / 2 + gridLen / 2 + lcdPad, boxDepth / 2]}>
+        <boxGeometry args={[gridLen, gridLen, lcdDepth]} />
+        <meshStandardMaterial color={"#CCCCCC"} />
       </mesh>
     </mesh>
   );
@@ -33,7 +65,7 @@ export function EZCheck() {
       <Canvas>
         <OrbitControls />
         <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+        <directionalLight color="white" position={[0, 0, 5]} />
         <Frame />
       </Canvas>
     </Flex>
