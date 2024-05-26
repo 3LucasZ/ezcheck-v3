8eaec024261d4143eca031c2755d5ba4e44dec01
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-26 03:09:11 -0700

    Home page overhaul!!!

--- src/pages/admin/home.tsx
+++ src/pages/admin/home.tsx
@@ -1,69 +1,144 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
-import { RouteButton } from "components/RouteButton";
-import Layout from "components/Layout/MainLayout";
-import { GetServerSideProps } from "next";
+import {
+  AbsoluteCenter,
+  Box,
+  Divider,
+  HStack,
+  Icon,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
-import prisma from "services/prisma";
 
-import { MdManageAccounts } from "react-icons/md";
-import { GiSewingMachine } from "react-icons/gi";
-import { IoDocumentText } from "react-icons/io5";
-import { IoIosInformationCircle, IoIosSettings } from "react-icons/io";
-import { BiSolidWrench } from "react-icons/bi";
-
-import { checkAdmin, getMyAdmin } from "services/userHandler";
-import { AdminProps } from "archive/AdminWidget2";
-import Header from "components/Layout/Header";
-import AppBar from "components/Layout/AppBar";
 import AdminLayout from "components/Layout/AdminLayout";
-import { SettingsIcon } from "@chakra-ui/icons";
-import { GrConfigure } from "react-icons/gr";
-import { FaWrench } from "react-icons/fa6";
+import { CustomStat } from "components/Main/CustomStat";
+import { group } from "console";
+import { FiUsers, FiCpu, FiAward, FiCompass, FiZap } from "react-icons/fi";
+import { responsivePx } from "services/constants";
+import prisma from "services/prisma";
+import { GetServerSideProps } from "next";
+import { User } from "next-auth";
+import { CertificateProps, MachineProps } from "types/db";
+import Carousel from "components/Main/Carousel";
+import CustomDivider from "components/Main/CustomDivider";
+import UserWidget from "components/Widget/UserWidget";
+import MachineWidget from "components/Widget/MachineWidget";
 
-export default function Home() {
+type PageProps = {
+  students: User[];
+  machines: MachineProps[];
+  certificates: CertificateProps[];
+};
+export default function Home(props: PageProps) {
   const { data: session } = useSession();
   const user = session?.user;
+  const machinesInUse = props.machines.filter(
+    (machine) => machine.usedBy != undefined && machine.usedBy != null
+  );
+  console.log(machinesInUse);
   return (
     <AdminLayout isAdmin={user?.isAdmin} isSupervisor={user?.isSupervising}>
-      <SimpleGrid
-        columns={[1, 2]}
-        overflowY="auto"
-        w="100svw"
-        h={"100%"}
-        position={"fixed"}
-        py={16}
-        top={"0%"}
-        zIndex={-100}
-      >
-        <RouteButton
-          route={"/admin/manage-students"}
-          text={"Manage Students"}
-          icon={MdManageAccounts}
-          color={"teal.300"}
-          hoverColor={"teal.100"}
-        ></RouteButton>
-        <RouteButton
-          route={"/admin/manage-machines"}
-          text={"Manage Machines"}
-          icon={GiSewingMachine}
-          color={"blue.300"}
-          hoverColor={"blue.100"}
-        ></RouteButton>
-        <RouteButton
-          route={"/admin/view-logs"}
-          text={"View Logs"}
-          icon={IoDocumentText}
-          color={"orange.400"}
-          hoverColor={"orange.100"}
-        ></RouteButton>
-        <RouteButton
-          route={"/admin/config"}
-          text={"Configure"}
-          icon={BiSolidWrench}
-          color={"red.400"}
-          hoverColor={"red.100"}
-        ></RouteButton>
-      </SimpleGrid>
+      <Box overflowY="auto">
+        <SimpleGrid
+          columns={3}
+          gap={[2, 4, 6]}
+          px={responsivePx}
+          py={[4, 6, 8]}
+        >
+          <CustomStat
+            label={"Students"}
+            value={"" + props.students.length}
+            link={`/admin/manage-students`}
+            icon={FiUsers}
+            dark="cyan.400"
+            med="cyan.200"
+            light="cyan.100"
+            xlight="cyan.50"
+          />
+          <CustomStat
+            label={"Machines"}
+            value={"" + props.machines.length}
+            link={`/admin/manage-machines`}
+            icon={FiCpu}
+            dark="blue.400"
+            med="blue.200"
+            light="blue.100"
+            xlight="blue.50"
+          />
+          <CustomStat
+            label={"Certificates"}
+            value={"" + props.certificates.length}
+            link={`/admin/manage-machines`}
+            icon={FiAward}
+            dark="orange.400"
+            med="orange.200"
+            light="orange.100"
+            xlight="orange.50"
+          />
+        </SimpleGrid>
+        <Box px={responsivePx}>
+          <CustomDivider
+            color="orange.300"
+            icon={FiZap}
+            text="Recently Created"
+          />
+          <Carousel
+            cards={[
+              ...props.students.map((student) => ({
+                image: student.image,
+                dbImage: false,
+                title: student.name,
+                url: "/admin/view-student/" + student.id,
+              })),
+              ...props.machines.map((machine) => ({
+                image: machine.image,
+                dbImage: true,
+                title: machine.name,
+                url: "/admin/view-machine/" + machine.id,
+              })),
+            ]}
+          />
+          <CustomDivider color="orange.300" icon={FiZap} text="In Use" />
+          <VStack w="100%">
+            {machinesInUse.length > 0 ? (
+              machinesInUse.map((machine) => (
+                <HStack minW="100%">
+                  <MachineWidget
+                    url={`/admin/view-machine/${machine.id}`}
+                    name={machine.name}
+                    description={machine.description}
+                    image={machine.image}
+                  />
+                  {/* <Text>{"< == >"}</Text> */}
+                  <UserWidget
+                    id={machine.usedBy.id}
+                    name={machine.usedBy.name}
+                    email={machine.usedBy.email}
+                    image={machine.usedBy.image}
+                  />
+                </HStack>
+              ))
+            ) : (
+              <Text>{"No data to display"}</Text>
+            )}
+          </VStack>
+        </Box>
+        <Box h="16"></Box>
+      </Box>
     </AdminLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const students = await prisma.user.findMany({ where: { isAdmin: false } });
+  const machines = await prisma.machine.findMany({ include: { usedBy: true } });
+  const certificates = await prisma.machineCertificate.findMany();
+
+  return {
+    props: {
+      students,
+      machines,
+      certificates,
+    },
+  };
+};
