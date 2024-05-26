96a3fe126dde56cde05b27996e09db086d57c636
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-26 03:08:43 -0700

    Create CustomDivider.tsx

---
+++ components/Main/CustomDivider.tsx
@@ -0,0 +1,29 @@
+import {
+  Box,
+  Divider,
+  AbsoluteCenter,
+  HStack,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { FiZap } from "react-icons/fi";
+
+type PageProps = {
+  color: string;
+  icon: IconType;
+  text: string;
+};
+export default function CustomDivider(props: PageProps) {
+  return (
+    <Box position="relative" py={8} px={2}>
+      <Divider />
+      <AbsoluteCenter bg="white">
+        <HStack color={props.color} px="3">
+          <Icon as={props.icon} boxSize={6} />
+          <Text fontSize="xl">{props.text}</Text>
+        </HStack>
+      </AbsoluteCenter>
+    </Box>
+  );
+}
