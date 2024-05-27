a58cfde8914854ab516f445594653d0b5703b41f
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-26 23:04:18 -0700

    Improve simulator again

--- src/pages/simulator.tsx
+++ src/pages/simulator.tsx
@@ -1,15 +1,21 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading, useBreakpointValue } from "@chakra-ui/react";
 import { EZCheckCanvas } from "components/EZCheckCanvas/EZCheckView";
 import Header from "components/Layout/Header";
 import Layout from "components/Layout/MainLayout";
 import { useSession } from "next-auth/react";
+import { responsivePx } from "services/constants";
 
 export default function Simulator() {
   const { data: session } = useSession();
   const user = session?.user;
+  const useBloom = useBreakpointValue({
+    base: false,
+    sm: true,
+  });
   return (
     <Layout>
       {/* <Header isAdmin={user?.isAdmin} /> */}
+
       <Heading
         textAlign={"center"}
         fontWeight={600}
@@ -18,9 +24,11 @@ export default function Simulator() {
         bgGradient={"linear(to-r, orange.300, red.400)"}
         bgClip={"text"}
       >
-        EZ-Check Simulator
+        EZCheck Simulator
       </Heading>
-      <EZCheckCanvas useBloom={true}></EZCheckCanvas>
+      <Box h="80%">
+        <EZCheckCanvas useBloom={useBloom}></EZCheckCanvas>
+      </Box>
     </Layout>
   );
 }
