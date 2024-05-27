b16ebc4a9932f6a3ba5d80f1d50ab81070bfe2fc
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-26 22:51:28 -0700

    Improved simulator page

--- src/pages/simulator.tsx
+++ src/pages/simulator.tsx
@@ -1,10 +1,26 @@
+import { Heading } from "@chakra-ui/react";
 import { EZCheckCanvas } from "components/EZCheckCanvas/EZCheckView";
+import Header from "components/Layout/Header";
 import Layout from "components/Layout/MainLayout";
+import { useSession } from "next-auth/react";
 
-export default function Test() {
+export default function Simulator() {
+  const { data: session } = useSession();
+  const user = session?.user;
   return (
     <Layout>
-      <EZCheckCanvas></EZCheckCanvas>
+      {/* <Header isAdmin={user?.isAdmin} /> */}
+      <Heading
+        textAlign={"center"}
+        fontWeight={600}
+        fontSize={["3xl", "4xl", "5xl"]}
+        lineHeight={"110%"}
+        bgGradient={"linear(to-r, orange.300, red.400)"}
+        bgClip={"text"}
+      >
+        EZ-Check Simulator
+      </Heading>
+      <EZCheckCanvas useBloom={true}></EZCheckCanvas>
     </Layout>
   );
 }
