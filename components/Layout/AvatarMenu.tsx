53f9467c422151844b7a5a5d5d1c58ffd3591231
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-28 22:10:11 -0700

    remove AvatarMenu badget

--- components/Layout/AvatarMenu.tsx
+++ components/Layout/AvatarMenu.tsx
@@ -27,12 +27,12 @@ export default function AvatarMenu({ isAdmin, isSupervisor }: AvatarMenuProps) {
           name={session?.user?.name ? session.user.name : ""}
           src={session?.user?.image ? session.user.image : ""}
         >
-          {isAdmin && (
+          {/* {isAdmin && (
             <AvatarBadge
               boxSize="1.25em"
               bg={isSupervisor ? "green.500" : "red.500"}
             />
-          )}
+          )} */}
         </Avatar>
       </MenuButton>
       <MenuList textAlign="left">
