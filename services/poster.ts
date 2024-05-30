d0777d0cc94570bc66d3e7e1c4ea39ff47b9cbbc
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-30 03:08:30 -0700

    poster return plain text option (returning json for a non-json endpoint will error)

--- services/poster.ts
+++ services/poster.ts
@@ -4,7 +4,8 @@ export async function poster(
   path: string,
   body: any,
   toaster?: any,
-  showSuccess?: boolean
+  showSuccess?: boolean,
+  plainText?: boolean
 ): Promise<Response> {
   try {
     const res = await fetch(path, {
@@ -13,7 +14,11 @@ export async function poster(
       body: JSON.stringify(body),
     });
     if (res.status != 200) {
-      if (toaster) errorToast(toaster, "" + (await res.json()));
+      if (toaster)
+        errorToast(
+          toaster,
+          "" + (plainText ? await res.text() : await res.json())
+        );
     } else {
       if (toaster && showSuccess) successToast(toaster, "Success!"); // Only if you want to show a success toast. However, they can get annoying to the user.
     }
