e24987a9b121a6b4040462faf4a6c914500709b6
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-30 17:14:02 -0700

    Update InviteEmail.tsx

--- components/InviteEmail.tsx
+++ components/InviteEmail.tsx
@@ -28,7 +28,6 @@ export default function InviteEmail(props: InviteEmailProps) {
   const baseUrl = "https://localhost:3000";
 
   //mid
-  const invitedByEmail = "alan.turing@example.com";
   const teamName = "Enigma";
   const teamImage = `${baseUrl}/images/logo.png`;
   const inviteLink = `${baseUrl}/`;
@@ -57,42 +56,25 @@ export default function InviteEmail(props: InviteEmailProps) {
               Hello {props.receiverEmail},
             </Text>
             <Text className="text-black text-[14px] leading-[24px]">
-              <strong>{props.senderEmail}</strong> (
+              <strong>{props.senderName}</strong> (
               <Link
-                href={`mailto:${invitedByEmail}`}
+                href={`mailto:${props.senderEmail}`}
                 className="text-blue-600 no-underline"
               >
-                {invitedByEmail}
+                {props.senderEmail}
               </Link>
-              ) has invited you to the <strong>{teamName}</strong> team on{" "}
-              <strong>Vercel</strong>.
+              ) has created an account for you to to use{" "}
+              <strong>{"EZCheck"}</strong>, Valley Christian's official machine
+              shop managing platform.
+              {/* team on{" "}<strong>Vercel</strong>. */}
             </Text>
-            <Section>
-              <Row>
-                <Column align="center">
-                  <Img
-                    src={`${baseUrl}/static/vercel-arrow.png`}
-                    width="12"
-                    height="9"
-                    alt="invited you to"
-                  />
-                </Column>
-                <Column align="left">
-                  <Img
-                    className="rounded-full"
-                    src={teamImage}
-                    width="64"
-                    height="64"
-                  />
-                </Column>
-              </Row>
-            </Section>
+
             <Section className="text-center mt-[32px] mb-[32px]">
               <Button
-                className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
+                className="bg-orange-300 rounded text-white text-[14px] font-semibold no-underline text-center px-5 py-3"
                 href={inviteLink}
               >
-                Finish registration
+                Start Machining
               </Button>
             </Section>
             <Text className="text-black text-[14px] leading-[24px]">
@@ -103,15 +85,14 @@ export default function InviteEmail(props: InviteEmailProps) {
             </Text>
             <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
             <Text className="text-[#666666] text-[12px] leading-[24px]">
-              This invitation was intended for{" "}
+              This notification was intended for{" "}
               <span className="text-black">{props.receiverEmail}</span>. This
-              invite was sent from{" "}
+              notification was sent from{" "}
               <span className="text-black">{"Valley Christian Schools"}</span>{" "}
               located in{" "}
               <span className="text-black">{"San Jose, California"}</span>. If
-              you were not expecting this invitation, you can ignore this email.
-              If you are concerned about your account's safety, please reply to
-              this email to get in touch with us.
+              you were not expecting this notification, you can ignore this
+              email.
             </Text>
           </Container>
         </Body>
