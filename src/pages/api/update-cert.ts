9f92d1a423c7a37d93a12845099b44de16b21533
3LucasZ <lucas.zheng@warriorlife.net>
2024-06-01 14:36:28 -0700

    Create update-cert.ts

---
+++ src/pages/api/update-cert.ts
@@ -0,0 +1,37 @@
+import type { NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import prisma from "services/prisma";
+import { prismaErrHandler } from "services/prismaErrHandler";
+import { TypedRequestBody } from "types/req";
+import { authOptions } from "./auth/[...nextauth]";
+
+export default async function handle(
+  req: TypedRequestBody<{
+    machineId: number;
+    recipientId: string;
+    newNote: string;
+  }>,
+  res: NextApiResponse
+) {
+  //--API Protection--
+  const session = await getServerSession(req, res, authOptions);
+  if (!session?.user.isAdmin) return res.status(401).json("Unauthorized");
+  //op
+  const { machineId, recipientId, newNote } = req.body;
+  try {
+    const op = await prisma.machineCertificate.update({
+      where: {
+        machineId_recipientId: {
+          machineId,
+          recipientId,
+        },
+      },
+      data: {
+        note: newNote,
+      },
+    });
+    return res.status(200).json(op);
+  } catch (e) {
+    return res.status(500).json(prismaErrHandler(e));
+  }
+}
