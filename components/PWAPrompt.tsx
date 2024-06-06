27d2e59eb27f7d818acbe2867d4b745f69708999
3LucasZ <lucas.zheng@warriorlife.net>
2024-06-05 20:32:09 -0700

    Handle PWAPrompt showing/closing/not showing

--- components/PWAPrompt.tsx
+++ components/PWAPrompt.tsx
@@ -1,15 +1,40 @@
 import { Box, Icon, Text } from "@chakra-ui/react";
-import { FiPlusSquare } from "react-icons/fi";
+import { useState } from "react";
+import { FiPlusSquare, FiX } from "react-icons/fi";
 import { IoShareOutline } from "react-icons/io5";
 
 export default function PWAPrompt() {
+  const isIos = () => {
+    // const userAgent = window.navigator.userAgent.toLowerCase();
+    // return /iphone|ipad|ipod/.test(userAgent);
+    return (
+      [
+        "iPad Simulator",
+        "iPhone Simulator",
+        "iPod Simulator",
+        "iPad",
+        "iPhone",
+        "iPod",
+      ].includes(navigator.platform) ||
+      // iPad on iOS 13 detection
+      (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+    );
+  };
+  console.log("isIos:", isIos);
+  const isInStandaloneMode = () =>
+    "standalone" in window.navigator && window.navigator.standalone;
+  console.log("isStandalone:", isInStandaloneMode);
+  const isCancelled = () => localStorage.getItem("isCancelled") === "true";
+  const [invisible, setInvisible] = useState(
+    !isIos() || isInStandaloneMode() || isCancelled()
+  );
   return (
     <Box
       pos="fixed"
-      bottom="8px"
-      left="8px"
-      right="8px"
-      maxW="600px"
+      bottom={invisible ? "-80px" : "12px"}
+      left="12px"
+      right="12px"
+      maxW="400px"
       mx="auto"
       //bg="gray.200"
       bg="white"
@@ -19,6 +44,7 @@ export default function PWAPrompt() {
       px="2"
       py="1"
       zIndex={10000}
+      transition={"all 0.05s linear"}
     >
       <Text>
         To install this webapp on your device: tap
@@ -31,11 +57,36 @@ export default function PWAPrompt() {
           pb="2px"
         />
         {/* {" "} */}
-        and then scroll down to find Add to Homescreen
+        and then click "Add to Homescreen"
         {/* {" "} */}
         {/* <Icon as={FiPlusSquare} boxSize={6} verticalAlign={"bottom"} pb="0px" /> */}
         .
       </Text>
+      <Box
+        w="20px"
+        h="20px"
+        bg="red.400"
+        pos={"absolute"}
+        top={"-10px"}
+        right={"-10px"}
+        borderRadius={"full"}
+        alignItems={"center"}
+        onClick={() => {
+          setInvisible(!invisible);
+          localStorage.setItem("isCancelled", "true");
+        }}
+      >
+        <Icon
+          as={FiX}
+          color="white"
+          position={"absolute"}
+          m="auto"
+          left={0}
+          right={0}
+          top={0}
+          bottom={0}
+        />
+      </Box>
     </Box>
   );
 }
