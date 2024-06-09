1d31add592e72e06b63d74cd81bdeeadb33288d7
3LucasZ <lucas.zheng@warriorlife.net>
2024-06-08 21:14:56 -0700

    Fortify join/leave machine APIs

--- src/pages/api/post/join-machine.ts
+++ src/pages/api/post/join-machine.ts
@@ -4,121 +4,130 @@ import { debugMode } from "services/constants";
 import prisma from "services/prisma";
 import { prismaErrHandler } from "services/prismaErrHandler";
 import { serverCreateLog } from "../create-log";
+import { TypedRequestBody } from "types/req";
 
+//IMPORTANT RULE #1: all response messages must be short because they have to be
+//displayed on a tiny LCD screen.
+//IMPORTANT RULE #2: do not change the names in the TypedRequestBody. The elements
+//machineName, etc are hard coded into the EZCheck modules already and it will be
+//a huge pain to change them all one by one.
 export default async function handle(
-  req: NextApiRequest,
+  req: TypedRequestBody<{
+    machineName: string;
+    studentPIN: string;
+    IP: string;
+  }>,
   res: NextApiResponse
 ) {
-  const { machineName, userPIN, IP } = req.body;
-
-  //find machine
-  const machine = await prisma.machine.findUnique({
-    where: {
-      name: machineName,
-    },
-    include: {
-      usedBy: true,
-    },
-  });
-  //find user
-  const user = await prisma.user.findUnique({
-    where: {
-      PIN: userPIN,
-    },
-    include: {
-      certificates: {
-        include: {
-          machine: true,
-        },
+  const { machineName, studentPIN, IP } = req.body;
+  try {
+    //find machine
+    const machine = await prisma.machine.findUnique({
+      where: {
+        name: machineName,
       },
-      using: true,
-    },
-  });
-  //find supervisors
-  const supervisors = await prisma.user.findMany({
-    where: {
-      isSupervising: true,
-    },
-  });
-  const supervisorsMsg = supervisors.length
-    ? "Supervisors: " +
-      supervisors.map((supervisor) => supervisor.email).join(", ") +
-      "."
-    : "No supervisors available.";
-  //find user allowed machines
-  const allowedMachineNames = user
-    ? user.certificates.map((cert) => cert.machine.name)
-    : [];
+      include: {
+        usedBy: true,
+      },
+    });
+    //find user
+    const user = await prisma.user.findUnique({
+      where: {
+        PIN: studentPIN,
+      },
+      include: {
+        certificates: {
+          include: {
+            machine: true,
+          },
+        },
+        using: true,
+      },
+    });
+    //find supervisors
+    const supervisors = await prisma.user.findMany({
+      where: {
+        isSupervising: true,
+      },
+    });
+    const supervisorsMsg = supervisors.length
+      ? "Supervisors: " +
+        supervisors.map((supervisor) => supervisor.email).join(", ") +
+        "."
+      : "No supervisors available.";
+    //find user allowed machines
+    const allowedMachineNames = user
+      ? user.certificates.map((cert) => cert.machine.name)
+      : [];
 
-  //check cases
-  if (
-    machine == null ||
-    user == null ||
-    IP == null ||
-    supervisors.length == 0
-  ) {
-    serverCreateLog(
-      (user == null ? "An unknown user" : user.name) +
-        " might be trespassing on " +
-        (machine == null
-          ? "an unknown machine (" + machineName + ") "
-          : machine?.name) +
-        " with IP " +
-        (IP == null ? "that is hidden" : IP) +
-        ". " +
-        supervisorsMsg,
-      2
-    );
-    if (machine == null) {
-      return res.status(500).send(machineName + " doesn't exist");
-    }
-    if (user == null) {
-      return res.status(500).send("Wrong PIN");
-    }
-    if (IP == null) {
-      return res.status(500).send("Empty IP");
-    }
-    if (supervisors.length == 0) {
-      return res.status(500).send("Unsupervised");
-    }
-  } else if (user.using != null) {
-    serverCreateLog(
-      user.name +
-        " is trying to use " +
-        machine.name +
-        ", but is already using " +
-        user.using.name +
-        ". " +
-        supervisorsMsg,
-      2
-    );
-    return res.status(500).send("Already using " + user.using.name);
-  } else if (machine.usedBy != null) {
-    serverCreateLog(
-      user.name +
-        " is trying to use " +
-        machine.name +
-        ", but it is already in use by " +
-        machine.usedBy.name +
-        ". " +
-        supervisorsMsg,
-      2
-    );
-    return res
-      .status(500)
-      .send(machine.name + " already in use by " + machine.usedBy.name + ".");
-  } else if (!allowedMachineNames.includes(machineName)) {
-    serverCreateLog(
-      user.name +
-        " is trying to use " +
-        machine.name +
-        ", but is not authorized. " +
-        supervisorsMsg,
-      2
-    );
-    return res.status(500).send("Not authorized");
-  } else {
-    try {
+    //check cases
+    if (
+      machine == null ||
+      user == null ||
+      IP == null ||
+      supervisors.length == 0
+    ) {
+      serverCreateLog(
+        (user == null ? "An unknown user" : user.name) +
+          " might be trespassing on " +
+          (machine == null
+            ? "an unknown machine (" + machineName + ") "
+            : machine?.name) +
+          " with IP " +
+          (IP == null ? "that is hidden" : IP) +
+          ". " +
+          supervisorsMsg,
+        2
+      );
+      if (machine == null) {
+        return res.status(500).send(machineName + " doesn't exist");
+      }
+      if (user == null) {
+        return res.status(500).send("Wrong PIN");
+      }
+      if (IP == null) {
+        return res.status(500).send("Empty IP");
+      }
+      if (supervisors.length == 0) {
+        return res.status(500).send("Unsupervised");
+      }
+    } else if (user.using != null) {
+      serverCreateLog(
+        user.name +
+          " is trying to use " +
+          machine.name +
+          ", but is already using " +
+          user.using.name +
+          ". " +
+          supervisorsMsg,
+        2
+      );
+      return res.status(500).send("Already using " + user.using.name);
+    } else if (machine.usedBy != null) {
+      serverCreateLog(
+        user.name +
+          " is trying to use " +
+          machine.name +
+          ", but it is already in use by " +
+          machine.usedBy.name +
+          ". " +
+          supervisorsMsg,
+        2
+      );
+      return res
+        .status(500)
+        .send(machine.name + " already in use by " + machine.usedBy.name + ".");
+    } else if (!allowedMachineNames.includes(machineName)) {
+      serverCreateLog(
+        user.name +
+          " is trying to use " +
+          machine.name +
+          ", but is not authorized. " +
+          supervisorsMsg,
+        2
+      );
+      return res.status(500).send("Not authorized");
+    } else {
       await prisma.machine.update({
         where: {
           name: machineName,
@@ -134,9 +143,11 @@ export default async function handle(
         0
       );
       return res.status(200).send(user.name);
-    } catch (e) {
-      serverCreateLog("Database error: " + prismaErrHandler(e), 2);
-      return res.status(500).send("Internal error");
     }
+  } catch (e) {
+    //serverCreateLog("Database error: " + prismaErrHandler(e), 2);
+    console.log(e); //print server logs to container console logs
+    serverCreateLog("[join-machine] database error", 2);
+    return res.status(500).send("Internal error");
   }
 }
--- src/pages/api/post/leave-machine.ts
+++ src/pages/api/post/leave-machine.ts
@@ -3,58 +3,65 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "services/prisma";
 import { prismaErrHandler } from "services/prismaErrHandler";
 import { serverCreateLog } from "../create-log";
+import { TypedRequestBody } from "types/req";
 
+//IMPORTANT RULE #1: all response messages must be short because they have to be
+//displayed on a tiny LCD screen.
+//IMPORTANT RULE #2: do not change the names in the TypedRequestBody. The elements
+//machineName, etc are hard coded into the EZCheck modules already and it will be
+//a huge pain to change them all one by one.
 export default async function handle(
-  req: NextApiRequest,
+  req: TypedRequestBody<{
+    machineName: string;
+  }>,
   res: NextApiResponse
 ) {
   const { machineName } = req.body;
-
-  //find machine
-  const machine = await prisma.machine.findUnique({
-    where: {
-      name: machineName,
-    },
-    include: {
-      usedBy: true,
-    },
-  });
-  //find user
-  const user = machine?.usedBy;
-  //find supervisors
-  const supervisors = await prisma.user.findMany({
-    where: {
-      isSupervising: true,
-    },
-  });
-  const supervisorsMsg = supervisors.length
-    ? "Supervisors: " +
-      supervisors.map((supervisor) => supervisor.email).join(", ") +
-      "."
-    : "No supervisors available.";
-  //check cases
-  if (user == null || machine == null || supervisors.length == 0) {
-    serverCreateLog(
-      (user == null ? "An unknown user" : user.name) +
-        " might be trespassing on " +
-        (machine == null
-          ? "an unknown machine (" + machineName + ") "
-          : machine?.name) +
-        ". " +
-        supervisorsMsg,
-      2
-    );
-    if (machine == null) {
-      return res.status(500).send(machineName + " doesn't exist");
-    }
-    if (user == null) {
-      return res.status(500).send("Already logged out");
-    }
-    if (supervisors.length == 0) {
-      return res.status(500).send("Unsupervised");
-    }
-  } else {
-    try {
+  try {
+    //find machine
+    const machine = await prisma.machine.findUnique({
+      where: {
+        name: machineName,
+      },
+      include: {
+        usedBy: true,
+      },
+    });
+    //find user
+    const user = machine?.usedBy;
+    //find supervisors
+    const supervisors = await prisma.user.findMany({
+      where: {
+        isSupervising: true,
+      },
+    });
+    const supervisorsMsg = supervisors.length
+      ? "Supervisors: " +
+        supervisors.map((supervisor) => supervisor.email).join(", ") +
+        "."
+      : "No supervisors available.";
+    //check cases
+    if (user == null || machine == null || supervisors.length == 0) {
+      serverCreateLog(
+        (user == null ? "An unknown user" : user.name) +
+          " might be trespassing on " +
+          (machine == null
+            ? "an unknown machine (" + machineName + ") "
+            : machine?.name) +
+          ". " +
+          supervisorsMsg,
+        2
+      );
+      if (machine == null) {
+        return res.status(500).send(machineName + " doesn't exist");
+      }
+      if (user == null) {
+        return res.status(500).send("Already logged out");
+      }
+      if (supervisors.length == 0) {
+        return res.status(500).send("Unsupervised");
+      }
+    } else {
       await prisma.machine.update({
         where: {
           name: machineName,
@@ -72,9 +79,11 @@ export default async function handle(
         0
       );
       return res.status(200).send("Logged out");
-    } catch (e) {
-      serverCreateLog("Database error: " + prismaErrHandler(e), 2);
-      return res.status(500).send("Internal error");
     }
+  } catch (e) {
+    // serverCreateLog("Database error: " + prismaErrHandler(e), 2);
+    console.log(e); //print server logs to container console logs
+    serverCreateLog("[leave-machine] database error", 2);
+    return res.status(500).send("Internal error");
   }
 }
